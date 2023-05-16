0250ea55f83e8ac37acd59085dd58461e09fa7f6
Dahlia-Belle Coleman <130016783+dahliasc@users.noreply.github.com>
2023-05-15 18:14:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -28,40 +28,29 @@ const btn = document.getElementById('getWeatherBtn');
 btn.addEventListener("click", () => {
   //Inside the btn.addEventListener, add this template:
   //------------------CURRENT WESTHER FORECAST--------------------------------------------------------
+  
   const xhr = new XMLHttpRequest(); //defines XML object
   xhr.open("GET", `http://localhost:3000/weather/${latitude}/${longitude}`); // this opens a get request for webiste
   xhr.send(); //this sends request "executes"
   xhr.onload = function() {
   
-   const body = JSON.parse(xhr.responseText)//What’s wrong w/ this (Think about the format responseText is in and what format we need it in (stringify/parse)
+   const body = JSON.parse(xhr.responseText)//Whatâ€™s wrong w/ this (Think about the format responseText is in and what format we need it in (stringify/parse)
 
-   let temp = body.temp
+   let temp = body.temperature
    let weatherStatus = body.weatherStatus
    document.getElementById("temperature").innerHTML = `Temperature: ${temp}°F`; //setting html variables to variables that we get
    document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`;
-   let city = 'Santa Cruz';
-   let finalString = `In ${city} it is ${temp}`;
-
-   finalString = xhr.open(`http://localhost:3000/weather/${latitude}/${longitude}`)
   }
   //----------------FORECAST fiveday forecast data (xhr2)-------------------------------------------------
   const xhr2 = new XMLHttpRequest();
-  xhr2.open("GET", `http://localhost:3000/5day/${lat}/${lon}`);
+  xhr2.open("GET", `http://localhost:3000/5day/${latitude}/${longitude}`);
   xhr2.send();
   xhr2.onload = function() {
-	const body = JSON.parse(xhr.responseText);
-	let forecast = body.forecast;
+	const body = JSON.parse(xhr2.responseText);
+	let forecast = body;
   let forecastElements = document.getElementsByClassName("forecast");
 	for (let i = 0; i < forecast.length; i++) {
-		forecastElements[i].innerHTML = `${forecast[i].dayName}: ${forecast[i].temp}°F`;
+    forecastElements[i].innerHTML = forecast[i].dayName + ": " + forecast[i].temp + "\u00B0F";
   }
 }
-
-//api key 915ece8a2a57e9341116d7f79f739833
-let forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]; //nested array of day: forecast pairs
-let forecastElements = document.getElementsByClassName("forecast"); //setting forecast elements to an array of divs with the class of 'forecast'
-  for (let i = 0; i < forecast.length; i++) { // For loop goes through 0th index to the lenghth
-      forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F"; // adding corresponding i'th forecast div element to HTML in the format of "Day: Temp F"
-    }
-      
 });
